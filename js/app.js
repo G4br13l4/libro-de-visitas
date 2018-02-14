37e6558eb28dbfd6312421efd9fcb4bd16be5c5c
G4br13l4 <gabolf93@gmail.com>
2018-02-14 11:59:23 -0600

    mostrar texto en textarea simultaneamente

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
+$( document ).ready(function() {
+    
+    $("#messageEntered").keyup(function() {
+        
+        $(messagePrevisual).text($("#messageEntered").val());
+    
+    });
 
+});
     
